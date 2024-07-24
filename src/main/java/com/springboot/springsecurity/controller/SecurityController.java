af5f28391912638acbebb72dfd2ecf2ec0ea4e26
Lucas Schlestein <lschlestein@gmail.com>
2024-07-24 11:21:26 -0300

    ADD sample without DB

---
+++ src/main/java/com/springboot/springsecurity/controller/SecurityController.java
@@ -0,0 +1,9 @@
+package com.springboot.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SecurityController {
+
+}
---
+++ src/main/java/com/springboot/springsecurity/security/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.springboot.springsecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/","/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout)->logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("123456")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+}
